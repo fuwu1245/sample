02f937aa747a0baec34afa41b613023f27634933
fuwu1245 <fuwu1245@gmail.com>
2017-01-28 17:30:42 +0800

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layout/default')
+@section('title', '关于我们页面标题')
+@section('content')
+  <h1>关于页面</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layout/default')
+@section('title', '帮助页面标题')
+@section('content')
+  <h1>帮助页面</h1>
+@stop
