aa24ea2d88f4a82f009bd0fd9b20d6f344f58579
fuwu1245 <fuwu1245@gmail.com>
2017-02-07 11:53:00 +0800

    Add user statuses

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,15 @@ class User extends Model implements AuthenticatableContract,
         });
     }
 
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
+
+    public function feed()
+    {
+        return $this->statuses()
+                    ->orderBy('created_at', 'desc');
+    }
+
 }
